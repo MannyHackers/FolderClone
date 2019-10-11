5e5f5bc639316e82ba884fc166d00cfd774f6d6e
Mz <mz@local>
2019-10-11 01:29:06 -0700

    organization

--- setup.py
+++ setup.py
@@ -1,25 +1,35 @@
-import setuptools
+from sys import platform
+from setuptools import setup,find_packages
+
+install_requires = [
+    'google_auth_oauthlib',
+    'urllib3',
+    'httplib2shim',
+    'protobuf',
+    'google_api_python_client']
+
+if platform == 'win32':
+    install_requires.append('pyreadline')
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
      name='folderclone',
-     version='0.5.0',
+     entry_points={
+        'console_scripts': [
+            'multimanager=folderclonecli.mmparse:main',
+            'multifolderclone=folderclonecli.mfcparse:main']
+    },
+     version='0.5.0b1',
      author='Spazzlo',
      description='A tool to copy large folders to Shared Drives.',
      long_description=long_description,
      long_description_content_type='text/markdown',
      url='https://github.com/Spazzlo/folderclone',
-     packages=['folderclone'],
-     install_requires=[
-        'pyreadline',
-        'google_auth_oauthlib',
-        'urllib3',
-        'httplib2shim',
-        'protobuf',
-        'google_api_python_client'
-    ],
+     packages=find_packages('src'),
+     package_dir={'':'src'},
+     install_requires=install_requires,
      classifiers=[
          'Programming Language :: Python :: 3',
          'License :: OSI Approved :: MIT License',
