7c424455501e6c86ca913928ee627d878d6d6b10
eXhumer <46933077+eXhumer@users.noreply.github.com>
2019-10-20 11:04:49 -0600

    Bug Fix for #77

--- src/folderclone/multimanager.py
+++ src/folderclone/multimanager.py
@@ -253,6 +253,7 @@ class multimanager():
                     successful.append(i['response']['emailAddress'])
 
     def remove_users(self,drive_id,emails=None,role=None,prefix=None,suffix=None):
+        to_be_removed = []
         if emails is None and role is None and prefix is None and suffix is None:
             raise ValueError('You must provide one of three options: role, prefix, suffix')
         all_perms = []
